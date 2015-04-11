38a5eb18cce210ff1a30feaaa7b54bf967438b1e
Stefan Scherer <scherer_stefan@icloud.com>
2015-04-11 10:11:44 +0200

    rainbow cycle

--- server.js
+++ server.js
@@ -10,49 +10,52 @@ ws281x.init(NUM_LEDS);
 
 var lightsOff = function () {
   for (var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = rgb2Int(0, 0, 0);
+    pixelData[i] = color(0, 0, 0);
   }
   ws281x.render(pixelData);
   ws281x.reset();
 }
 
-// ---- trap the SIGINT and reset before exit
-process.on('SIGINT', function () {
-  lightsOff();
-  console.log('got SIGINT');
-  process.nextTick(function () { process.exit(0); });
-});
+var signals = {
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
 
-process.on('SIGTERM', function () {
-  console.log('got SIGTERM');
+function shutdown(signal, value) {
+  console.log('Stopped by ' + signal);
   lightsOff();
   process.nextTick(function () { process.exit(0); });
-});
+}
 
+Object.keys(signals).forEach(function (signal) {
+  process.on(signal, function () {
+    shutdown(signal, signals[signal]);
+  });
+});
 
 // ---- animation-loop
 var offset = 0;
 setInterval(function () {
   for (var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = colorwheel((offset + i) % 256);
+    pixelData[i] = wheel(((i * 256 / NUM_LEDS) + offset) % 256);
   }
 
   offset = (offset + 1) % 256;
   ws281x.render(pixelData);
 }, 1000 / 30);
 
-console.log('Press <ctrl>+C to exit.');
+console.log('Rainbow started. Press <ctrl>+C to exit.');
 
-
-// rainbow-colors, taken from http://goo.gl/Cs3H0v
-function colorwheel(pos) {
+// generate rainbow colors accross 0-255 positions.
+function wheel(pos) {
   pos = 255 - pos;
-  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
+  if (pos < 85) { return color(255 - pos * 3, 0, pos * 3); }
+  else if (pos < 170) { pos -= 85; return color(0, pos * 3, 255 - pos * 3); }
+  else { pos -= 170; return color(pos * 3, 255 - pos * 3, 0); }
 }
 
-function rgb2Int(r, g, b) {
+// generate integer from RGB value
+function color(r, g, b) {
   r = r * brightness / 255;
   g = g * brightness / 255;
   b = b * brightness / 255;
